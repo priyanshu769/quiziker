4c34ed7e0c654894d305a241a90f55594c844933
Priyanshu <prynsu@yahoo.com>
2021-05-23 17:01:35 +0530

    Fix: App title and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Quizzes from "./pages/quizzes";
 import PlayArea from "./pages/playArea";
 import FinalScore from "./pages/finalScore";
@@ -10,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Box bg="#3fc1c9" w="100%" p={4} marginBottom="1rem" color="black">
-      <Text fontSize="6xl">Quiziker</Text>
+        <Text fontSize="6xl">Quiziker</Text>
       </Box>
       <Routes>
         <Route path="/" element={<Quizzes />} />
